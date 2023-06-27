426df5e8490a5fe1faba53a4966f73b32f279eda
Fernanda <136729254+feninvuwu@users.noreply.github.com>
2023-06-27 10:44:18 -0400

    Add files via upload

---
+++ riquelme_yulissa_pgy1121_p3_008d.py.py
@@ -0,0 +1,84 @@
+import
+
+def validar_menu_mascota
+
+print("""
+1.Rut del dueño
+2.nombre del dueño
+3.identificador unico
+4.nombre de la mascota
+5.cantidad de dias en guarderia
+6.salir""")
+
+if  validar == 1;
+
+while True: 
+try
+
+opcion(input(print("ESCOJA UNA OPCIÓN")))
+
+if_opcion
+
+else:
+
+print("ERROR! DEBE SELECCIONAR UNA OPCIÓN")
+
+except:
+
+    print("SU OPCIÓN ES INCORRECTA!")
+
+
+except:
+    print("SU OPCIÓN ES CORRECTA!")
+
+
+
+validar_opcion
+
+while true:
+
+def validar_cantidad:
+
+def validar_nombre_mascota:
+
+def validar_identificador:
+
+def validar_can_dias:
+
+def validar_nombre_dueno:
+
+def validar_rut_dueno:
+
+
+    def validar_rut:
+while true:
+    try:
+        rut=int(input("Ingrese su rut, sin punto, ni número digitador."))
+        if rut == 11111111
+else: 
+    ¡ERROR, DEBE ESTAR ENTRE 100000 o 999999! 
+     
+     except:
+        ¡ERROR, DEBE CONSTITUIR COMO NÚMERO ENTERO!
+        
+
+def validar_nombre:
+    while true 
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   ¡GRACIAS POR PREFERIR A MASCOTA SEGURA!
+
+
+
+
